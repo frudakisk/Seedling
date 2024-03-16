1a26ae62e8f942cd68a75602649f082b93e5195e
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2024-03-16 15:33:40 -0600

    space from waterhole good
    I need to make sure the water holes are not hidden behind rocks so that the player can get them. Now there is a possibility for a buffer between all rocks and a water hole and it is up to game design to determine how large this space should be. This functionality should not make the game slow at all

--- Seedling/Assets/Scripts/GameManager.cs
+++ Seedling/Assets/Scripts/GameManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> rockObjects;
+    public GameObject waterPocket;
     public int numRocks;
     public float xRange;
     public float yHeight;
+
+    private WaterHoleBehaviour[] waterHoles;
+    public int numWaterHoles;
 
     public static bool gameOver;
     public static bool gameOn;
@@ -16,6 +20,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //spawn water pockets first
+        SpawnWaterHoles();
+        waterHoles = FindAllActiveWaterPockets();
         SpawnRocks();
     }
 
@@ -29,11 +36,26 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void SpawnWaterHoles()
+    {
+        for(int i = 0; i < numWaterHoles; i++)
+        {
+            Vector3 randPos = new Vector3(Random.Range(-xRange, xRange), Random.Range(0, yHeight), 0);
+            Instantiate(waterPocket, randPos, Quaternion.identity);
+        }
+    }
+
     private void SpawnRocks()
     {
         for (int i = 0; i < numRocks; i++)
         {
-            Vector3 randPos = new Vector3(Random.Range(-xRange, xRange), Random.Range(0, yHeight), 0);
+            //rock position is dependent on the water pocket positions
+            Vector3 randPos;
+            do
+            {
+                randPos = new Vector3(Random.Range(-xRange, xRange), Random.Range(0, yHeight), 0);
+            } while (isNearWater(randPos));
+            
             int randomRock = Random.Range(0, rockObjects.Count);
             GameObject rock = Instantiate(rockObjects[randomRock], randPos, Quaternion.identity);
             float randomRotation = Random.Range(0, 360f);
@@ -41,5 +63,29 @@ public class GameManager : MonoBehaviour
             float randomeScale = Random.Range(0.5f, 3f);
             rock.transform.localScale = new Vector3(randomeScale, randomeScale, randomeScale);
         }
+    }
+
+    private bool isNearWater(Vector3 pos)
+    {
+        //need to find all water pockets that are active in the scene
+        Debug.Log($"Num of water holes: {waterHoles.Length}");
+        foreach(WaterHoleBehaviour obj in waterHoles)
+        {
+            Debug.Log(Vector3.Distance(obj.transform.position, pos));
+            if(Vector3.Distance(obj.transform.position, pos) < 3.0f)
+            {
+                //Too close to a waterhole
+                Debug.Log("Too Close. Create a new distance");
+                return true;
+            }
+        }
+        Debug.Log("Far enough from all water holes");
+        return false;
+
+    }
+
+    private WaterHoleBehaviour[] FindAllActiveWaterPockets()
+    {
+        return FindObjectsOfType<WaterHoleBehaviour>();
     }
 }
